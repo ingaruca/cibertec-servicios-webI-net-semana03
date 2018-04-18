18d1d2c79bfa3c14de1f35c4a9bd245538de03f0
Luis Ingaruca <luise.ingaruca@gmail.com>
2018-04-18 00:02:03 -0500

    Agregando proyecto asp net semana03

---
+++ Semana03/Controllers/NegociosController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Semana03.Models;
+
+namespace Semana03.Controllers
+{
+    public class NegociosController : Controller
+    {
+        Negocios2018Entities bd = new Negocios2018Entities();
+        // GET: Negocios
+        public ActionResult Index()
+        {
+            return View(bd.clientes.ToList());
+        }
+
+        public ActionResult BuscarClientexPais(string txtFiltroPais)
+        {
+            ViewBag.paises = new SelectList(bd.paises.ToList(), "Idpais", "NombrePais");
+            return View(bd.clientes.Where(c => c.idpais == txtFiltroPais).ToList());
+        }
+
+        public ActionResult PedidoxCliente(string txtCliente)
+        {
+            ViewBag.clientes = new SelectList(bd.clientes.ToList(), "IdCliente", "NomCliente");
+            return View(bd.pedidoscabe.Where(p => p.IdCliente == txtCliente).ToList());
+        }
+    }
+}
---
+++ Semana03/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Semana03.MvcApplication" Language="C#" %>
